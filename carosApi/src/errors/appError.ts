69ae79f1ef5e17958f6cdc073d5565a266adc188
Gabriel erick <102499159+GabrielErick1@users.noreply.github.com>
2025-04-09 18:37:13 -0300

    Delete carosApi directory
    aa

--- carosApi/src/errors/appError.ts
+++
@@ -1,8 +0,0 @@
-export class AppError {
-    public message: string;
-    public statusCode: number;
-    constructor(message: string, statusCode = 400) { 
-        this.statusCode = statusCode;
-        this.message = message;
-    }
-}
--- carosApi/src/routers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './CategoryTouters';
-export * from "./AutentiucateRouters"
-export * from "./especializationRouters"
-export * from "./routersUsers"
